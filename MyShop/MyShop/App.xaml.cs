6127d5b10b008360ad3003d03340940b29c4751e
Beniamino Negrini <beniamino.negrini@nttdata.com>
2018-02-20 16:56:23 +0100

    Add project files.

---
+++ MyShop/MyShop/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using MyShop.Views;
+using Xamarin.Forms;
+
+namespace MyShop
+{
+	public partial class App : Application
+	{
+
+		public App ()
+		{
+			InitializeComponent();
+
+
+            MainPage = new MainPage();
+        }
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
